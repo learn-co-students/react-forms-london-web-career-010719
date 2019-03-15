75d147a1088f9b48e5ab4bdd014c91b30bf24446
studioshane <studioshane@gmail.com>
2019-03-15 15:10:38 +0000

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,11 @@
-// Code ControlledInput Component Here
+import React from "react";
+
+export default class ControlledInput extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="password" onChange={e => console.log(e.target.value)} />
+      </form>
+    );
+  }
+}
